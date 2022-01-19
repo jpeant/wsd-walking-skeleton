5840653c6027be960e58017bc046ac6b31fefc72
Juha Antikainen <60448638+jpeant@users.noreply.github.com>
2022-01-19 19:07:05 +0200

    Update app.js
    new response "i will...",
    removed "hello...".

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
 import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
 
 const handleRequest = (request) => {
-  console.log(`Request to ${request.url}`);
-  return new Response("Hello world!");
+  return new Response("I will learn how to write web applications!");
 };
 
-console.log("Launching server on port 7777");
 serve(handleRequest, { port: 7777 });
