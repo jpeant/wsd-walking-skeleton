b9acb6b6706084db74718e4b18df24722e144680
Juha Antikainen <jussi_antikainen@hotmail.com>
2022-02-24 16:48:52 +0200

    update

--- app/app.js
+++ app/app.js
@@ -1,7 +1,35 @@
 import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
 
+// sum', 'difference', 'product', and 'quotient',
+// +        -               x             :
+// `http://localhost:7777?operation=product&number1=2&number2=2`
+
 const handleRequest = (request) => {
-  return new Response("I will learn how to write web applications!");
+  const url = new URL(request.url);
+  const params = url.searchParams;
+
+  const calculate = (a) =>
+    (operator) =>
+      (b) => {
+        switch (operator) {
+          case "sum":
+            return Number(a) + Number(b);
+          case "difference":
+            return a - b;
+          case "product":
+            return a * b;
+          case "quotient":
+            return a / b;
+          default:
+            return "Invalid parameters.";
+        }
+      };
+
+  return new Response(
+    calculate(params.get("number1"))(params.get("operation"))(
+      params.get("number2"),
+    ),
+  );
 };
 
 serve(handleRequest, { port: 7777 });
